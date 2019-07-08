1aefce2679add718efb4d9bc3a9e602782271ed0
Soleyu <soleyu.alpha@gmaill.com>
2019-07-08 17:12:04 -0500

    Collectibles
    Added Collectibles and scripts for that

--- 2d-ufo-unity/Assets/Scripts/PlayerController.cs
+++ 2d-ufo-unity/Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -7,11 +8,15 @@ public class PlayerController : MonoBehaviour
 
     public float speed;
     private Rigidbody2D rb2d;
+    private int count;
+    public Text CountText;
 
     // Start is called before the first frame update
     void Start()
     {
+        count = 0;
         rb2d = GetComponent<Rigidbody2D> ();
+        SetCountText ();
     }
 
     // Update is called once per frame
@@ -21,6 +26,19 @@ public class PlayerController : MonoBehaviour
         float moveVertical = Input.GetAxis ("Vertical");
         Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
         rb2d.AddForce (movement * speed);
+    }
 
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText ();
+        }
+    }
+
+    void SetCountText () 
+    {
+        CountText.text = "Gems: " + count.ToString();
     }
 }
