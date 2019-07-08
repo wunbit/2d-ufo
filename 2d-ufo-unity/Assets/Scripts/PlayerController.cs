78089cd84fee0fbb64a8a81d4be533a60961cb8e
Soleyu <soleyu.alpha@gmaill.com>
2019-07-08 17:36:50 -0500

    Win condition and restart
    Added Win text. Also Added Restart function which was not in tut

--- 2d-ufo-unity/Assets/Scripts/PlayerController.cs
+++ 2d-ufo-unity/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,6 +11,8 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb2d;
     private int count;
     public Text CountText;
+    private bool isOver = false;
+    public GameObject winText;
 
     // Start is called before the first frame update
     void Start()
@@ -22,10 +25,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (isOver == false)
+        {
         float moveHorizontal = Input.GetAxis ("Horizontal");
         float moveVertical = Input.GetAxis ("Vertical");
         Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
         rb2d.AddForce (movement * speed);
+        }
+        if (isOver == true && Input.GetKeyDown (KeyCode.Space))
+        {
+            SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+        }
     }
 
     void OnTriggerEnter2D(Collider2D other) {
@@ -40,5 +50,12 @@ public class PlayerController : MonoBehaviour
     void SetCountText () 
     {
         CountText.text = "Gems: " + count.ToString();
+        if (count >= 16)
+        {
+            rb2d.velocity = Vector2.zero;
+            isOver= true;
+            winText.SetActive(true);
+
+        }
     }
 }
